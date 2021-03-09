a49d59c2a2b88a138fa57a82d32cba7f213b057d
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-03-09 10:40:40 +0000

    [add]3-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0ffab71f45e8ae5fb7857ddab44e54e3ebc1cc38282dc07f4c0b4d884a96edccb36798a43a2808660fba93f41d564daab12af5702c1b457e1f62dc4ec1e8afcd'
