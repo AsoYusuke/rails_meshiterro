b3d3fe369e6049f77ff1c0943c4307d26567d359
AsoYusuke <aso.aso.1227@i.softbank.jp>
2021-03-10 13:18:37 +0000

    [add]3-5

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   has_many :post_images, dependent: :destroy
   has_many :post_connebts, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  attachment :profile_image
   
 end
